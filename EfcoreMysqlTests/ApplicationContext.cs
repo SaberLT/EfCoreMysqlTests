3d3f33606b5e2240b8bb5bb734b7cff50119f9a0
Dmitry Borisov <dmitry.borisov.itp@gmail.com>
2019-09-30 15:51:50 +0300

    Initial

---
+++ EfcoreMysqlTests/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfcoreMysqlTests
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseMySQL("server=localhost;UserId=root;Password=root;database=tests");
+        }
+    }
+}
---
+++ EfcoreMysqlTests/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EfcoreMysqlTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(ApplicationContext db= new ApplicationContext())
+            {
+                db.Database.EnsureCreated();
+
+                User user1 = new User { Age = 12, Name = "12" };
+                User user2 = new User { Age = 21, Name = "21" };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
